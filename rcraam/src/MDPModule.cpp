993470f2ee2b6ee23c280a55e85dd5ad1a788bb6
Marek Petrik <marekpetrik@gmail.com>
2017-10-25 09:36:17 -0400

    moved worstcasel1 method

--- rcraam/src/MDPModule.cpp
+++ rcraam/src/MDPModule.cpp
@@ -3,6 +3,7 @@
 #include "craam/modeltools.hpp"
 #include "craam/algorithms/values.hpp"
 
+#include "craam/fastopt.hpp"
 #include <iostream>
 
 using MDP = craam::MDP;
@@ -57,4 +58,22 @@ RCPP_MODULE(class_MDP) {
     .method("from_dataframe", &from_dataframe)
     .method("solve_mpi", &solve_mpi);
 
+}
+
+
+// [[Rcpp::export]]
+Rcpp::List worstcase_l1(Rcpp::NumericVector z, Rcpp::NumericVector q, double t){
+    // resulting probability
+    craam::numvec p;
+    // resulting objective value
+    double objective;
+
+    craam::numvec vz(z.begin(), z.end()), vq(q.begin(), q.end());
+    std::tie(p,objective) = craam::worstcase_l1(vz,vq,t);
+
+    Rcpp::List result;
+    result["p"] = Rcpp::NumericVector(p.cbegin(), p.cend());
+    result["obj"] = objective;
+
+    return result;
 }
