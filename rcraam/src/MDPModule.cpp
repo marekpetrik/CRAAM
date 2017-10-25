fc9e851003f0ef3b38933e8675393d126163de9d
Marek Petrik <marekpetrik@gmail.com>
2017-10-25 14:14:33 -0400

    removed duplicates

--- rcraam/src/MDPModule.cpp
+++ rcraam/src/MDPModule.cpp
@@ -61,19 +61,3 @@ RCPP_MODULE(class_MDP) {
 }
 
 
-// [[Rcpp::export]]
-Rcpp::List worstcase_l1(Rcpp::NumericVector z, Rcpp::NumericVector q, double t){
-    // resulting probability
-    craam::numvec p;
-    // resulting objective value
-    double objective;
-
-    craam::numvec vz(z.begin(), z.end()), vq(q.begin(), q.end());
-    std::tie(p,objective) = craam::worstcase_l1(vz,vq,t);
-
-    Rcpp::List result;
-    result["p"] = Rcpp::NumericVector(p.cbegin(), p.cend());
-    result["obj"] = objective;
-
-    return result;
-}
